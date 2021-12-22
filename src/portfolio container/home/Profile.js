1c518f90358bb3c3a9e3f42878aff99f84d54f75
Varun Prohit <20163@iiitu.ac.in>
2021-12-22 09:13:36 +0530

    first commit

---
+++ src/portfolio container/home/Profile.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function profile() {
+    return (
+        <div className='profile-container'>
+            <div className='profile-parent'>
+                <div className='profile-detail'>
+                    <div className='colz'>
+                        <a href='a'>
+                            <i className='fa fa-facebook-square'></i>
+                        </a>
+                        <a href='a'>
+                            <i className='fa fa-google-plus-square'></i>
+                        </a>
+                        <a href='a'>
+                            <i className='fa fa-instagram-square'></i>
+                        </a>
+                        <a href='a'>
+                            <i className='fa fa-youtube-square'></i>
+                        </a>
+                        <a href='a'>
+                            <i className='fa fa-twitter-square'></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
